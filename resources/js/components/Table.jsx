d65380c535b2d48c8626593ea62672934fd6a05e
KRIRLO <19690360@tecvalles.mx>
2022-10-13 02:41:33 -0500

    mostrar tabla Practica2

---
+++ resources/js/components/Table.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {BootstrapTable, 
+       TableHeaderColumn} from 'react-bootstrap-table';
+import '../css/Table.css';
+import '../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css'
+ 
+ 
+class Table1 extends Component {
+  render() {
+    return (
+      <div>
+        <BootstrapTable data={this.props.data}>
+          <TableHeaderColumn dataField='name'>
+            Name
+          </TableHeaderColumn>
+            <TableHeaderColumn dataField='email'>
+            Email
+          </TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
+  }
+}
+ 
+export default Table1;
